b9160b0d4ed0ac2dc10e435790d4c7e507c4b2b9
navi42chd-ai <navi42chd@gmail.com>
2026-07-24 12:40:48 +0530

    Add files via upload

--- js/subjects.js
+++ js/subjects.js
@@ -12,6 +12,12 @@ const SUBJECTS = [
         label: 'Stone Age',
         subtitle: '242 one-liner based MCQs',
         dataVar: 'chapter_stone_age'
+      },
+      {
+        id: 'indus-valley',
+        label: 'Indus Valley Civilisation',
+        subtitle: '331 one-liner based MCQs',
+        dataVar: 'chapter_indus_valley'
       }
     ]
   }
